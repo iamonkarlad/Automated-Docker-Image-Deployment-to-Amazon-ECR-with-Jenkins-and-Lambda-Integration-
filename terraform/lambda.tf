9c914d81b0e3406fd8fcd82d93aef41210ee4e94
iamonkarlad <ladonkar9@gmail.com>
2026-02-01 18:46:08 +0530

    added img uri

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_lambda_function" "ecr_lambda" {
     role          = aws_iam_role.lambda_role.arn
 
     package_type = "Image"
-    image_uri    = "1667824d0387.dkr.ecr.us-east-1.amazonaws.com/my-ecr-repo:latest"
+    image_uri    = "343218213278.dkr.ecr.us-east-1.amazonaws.com/my-ecr-repo:latest"
     
     timeout = 30
 }
